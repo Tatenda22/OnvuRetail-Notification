e4e6292caf4957be8268e231290e3ff6cc5a86c1
Tatenda22 <sigauketatenda5@gmail.com>
2018-08-15 14:14:34 +0100

    Initial commit of first test phase of the notification app

---
+++ NotifyTest1_Solution/NotifyTest1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace NotifyTest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PopupNotifier popup = new PopupNotifier();
+            popup.Image = Properties.Resources.info1;
+            popup.TitleText = "OnvuRetail Update Alert !";
+            popup.ContentText = "Camera updates required (Now or Later)";
+            popup.Popup(); // shows the nortification
+        }
+    }
+}
